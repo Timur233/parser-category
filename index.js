69b182327c727d997ab3a1dcbf9a29de21686d74
Iskandarov Timur <timur.iskandarov96@gmail.com>
2021-10-05 15:40:56 +0600

    Финальная версия

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//npm i cheerio
+//npm i axios
+
+const { data } = require('cheerio/lib/api/attributes');
+
 class CategoryParser {
 
     constructor(url) {
@@ -8,9 +13,11 @@ class CategoryParser {
 
         const axios = require('axios');
         const html = axios.get(this.url)
+        
         .then(function (response) {
             return response;
         })
+        
         .catch(function (error) {
             console.log(error);
         })
@@ -21,34 +28,30 @@ class CategoryParser {
 
     async getTable() {
 
-        var fs = require('fs');
+        const cheerio = require('cheerio');
+        const html = await this.getHTML();
 
-        fs.open('testFile.txt', 'w', (err) => {
-            if(err) throw err;
-            console.log('File created');
-        });
+        let getData = html => {
+            let data = [];
+            const $ = cheerio.load(html);
+            
+            (function parseTable() {
+                $('table.confluenceTable>tbody>tr:not(:first-child)').each((i, tr) => {
+                    
+                    let firstLevel = $(tr).find('td:first-child').text();
+                    let secondLevel = $(tr).find('td:nth-child(2)').text();
+                    let threeLevel = $(tr).find('td:nth-child(3)').text();
+                    let precent = $(tr).find('td:last-child').text();
 
-        const table = await this.getHTML();
-        const xpath = require('xpath');
-        const dom = require('xmldom').DOMParser;
- 
-        var xml = table.data;
-        var doc = new dom().parseFromString(xml, 'text/xml');
-        var nodes = xpath.select("//table[@class='wrapped relative-table confluenceTable']/tbody/*[preceding-sibling::tr[1]]", doc);
-        //console.log(nodes[0].toString());
-        //console.log(nodes);
-        // let tds = xpath.select("//td", nodes[56]);
-        // console.log(tds[0].toString());
-        nodes.forEach(tr => {
-            var row = new dom().parseFromString(tr.toString(), 'text/xml');
-            let tds = xpath.select("//td", row);
-            console.log('elem123ent', tds[1].lastChild.lastChild.data);
-            fs.appendFile('testFile.txt', tds[1].lastChild.lastChild.data + '\n', (err) => {
-                if(err) throw err;
-                console.log('Data has been added!');
-            });
-        })
-        //return nodes[0].localName + ": " + nodes[0].firstChild.data;
+                    data.push({firstLevel, secondLevel, threeLevel, precent});
+                    
+                })
+            })()
+            
+            return data;
+        }
+
+        return getData(html.data);
 
     }
 
@@ -57,6 +60,7 @@ class CategoryParser {
 async function startParser() {
     var parser = new CategoryParser('https://kaspi.kz/merchantcabinet/support/pages/viewpage.action?pageId=8912971');
     console.log(await parser.getTable());
+    return await parser.getTable();
 }
 
-startParser()
+startParser();
