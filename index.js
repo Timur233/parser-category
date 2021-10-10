26ecb9cff9fe46bac65fd0f82b9112521bb9eafe
Iskandarov Timur <timur.iskandarov96@gmail.com>
2021-10-11 02:04:33 +0600

    Допилил код товара к массиву

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-//npm i cheerio
-//npm i axios
-
-const { data } = require('cheerio/lib/api/attributes');
-
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
+import axios from 'axios';
 class CategoryParser {
 
-    constructor(url) {
-        this.url = url
+    constructor(getTariffsLink, getCategoriesLink, authToken) {
+        this.tariffsLink = getTariffsLink,
+        this.catsLink = getCategoriesLink,
+        this.authToken = authToken
     }
 
     async getHTML() {
-
-        const axios = require('axios');
-        const html = axios.get(this.url)
+        const html = axios.get(this.tariffsLink)
         
         .then(function (response) {
             return response;
         })
         
         .catch(function (error) {
-            console.log(error);
+            console.log('getHtml request', error);
         })
 
         return html;
@@ -28,8 +26,8 @@ class CategoryParser {
 
     async getTable() {
 
-        const cheerio = require('cheerio');
         const html = await this.getHTML();
+        const categories = await this.getKaspiCategories();
 
         let getData = html => {
             let data = [];
@@ -41,9 +39,11 @@ class CategoryParser {
                     let firstLevel = $(tr).find('td:first-child').text();
                     let secondLevel = $(tr).find('td:nth-child(2)').text();
                     let thirdLevel = $(tr).find('td:nth-child(3)').text();
+                    let categoryCode = categories[thirdLevel.replace(/\s+/g, '').toLowerCase()];
                     let precent = $(tr).find('td:last-child').text();
 
-                    data.push({firstLevel, secondLevel, thirdLevel, precent});
+                    data.push({firstLevel, secondLevel, thirdLevel, categoryCode, precent});
+                    updateCategoryInSvb(firstLevel, secondLevel, thirdLevel, categoryCode, precent);
                     
                 })
             })()
@@ -55,12 +55,52 @@ class CategoryParser {
 
     }
 
+    async getKaspiCategories() {
+        
+        var categoryCodes = {};
+        const categories = await fetch(this.catsLink, {
+            method: 'get',
+            headers: {
+                'X-Auth-Token': this.authToken,
+                'Accept': 'application/json',
+                'Host': 'kaspi.kz'
+            }
+        })
+
+        const categoriesJspn = await categories.json();
+        categoriesJspn.forEach(category => {
+            categoryCodes[category.title.replace(/\s+/g, '').toLowerCase()] = category.code;
+        });
+
+        return categoryCodes;
+
+    }
+
+    async updateCategoryInSvb(firstLevel, secondLevel, thirdLevel, categoryCode, precent) {
+        
+        const getCategoryUuid = (categoryTitle, parentUuid = '') => {
+            // Reuest where cat-title = categoryTitle and where parent-uuid = parentUuid
+            // Return UUID
+        }
+        var parent = '';
+
+        parent = getCategoryUuid(firstLevel);
+        parent = getCategoryUuid(secondLevel, parent);
+
+
+    }
+
 }
 
-async function startParser() {
-    var parser = new CategoryParser('https://kaspi.kz/merchantcabinet/support/pages/viewpage.action?pageId=8912971');
+(async function () {
+
+    const getTariffsLink = 'https://kaspi.kz/merchantcabinet/support/pages/viewpage.action?pageId=8912971';
+    const getCategoriesLink = 'https://kaspi.kz/shop/api/products/classification/categories';
+    const authToken = 'KdVO8SkcbqwKkCU70J1N/qYNKxQBR2YBsppaeoarnpQ=';
+
+    var parser = new CategoryParser(getTariffsLink, getCategoriesLink, authToken);
     console.log(await parser.getTable());
-    return await parser.getTable();
-}
 
-startParser();
+    return await parser.getTable();
+
+})()
